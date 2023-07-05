da6e9b6c157e3e785104f5282865a63250378df3
Andrey <SovereignAndrey>
2023-07-05 16:20:02 +0800

    new entities & handlers for multisigs & accounts;

--- src/ap-team-multi-sig.ts
+++ src/ap-team-multi-sig.ts
@@ -1,187 +1,125 @@
 import {
-  ApprovalsRequiredChanged as ApprovalsRequiredChangedEvent,
-  ConfirmationRevoked as ConfirmationRevokedEvent,
-  Deposit as DepositEvent,
   Initialized as InitializedEvent,
+  ApprovalsRequiredChanged as ApprovalsRequiredChangedEvent,
+  RequireExecutionChanged as RequireExecutionChangedEvent,
+  ExpiryChanged as ExpiryChangedEvent,
   OwnerAdded as OwnerAddedEvent,
   OwnerRemoved as OwnerRemovedEvent,
-  RequireExecutionChanged as RequireExecutionChangedEvent,
-  TransactionConfirmed as TransactionConfirmedEvent,
-  TransactionExecuted as TransactionExecutedEvent,
-  TransactionExpiryChanged as TransactionExpiryChangedEvent,
   TransactionSubmitted as TransactionSubmittedEvent
+  TransactionConfirmed as TransactionConfirmedEvent,
+  TransactionConfirmationRevoked as TransactionConfirmationRevokedEvent,
+  TransactionExecuted as TransactionExecutedEvent,
 } from "../generated/APTeamMultiSig/APTeamMultiSig"
 import {
-  ApprovalsRequiredChanged,
-  APTeamMultiSigDeposit,
-  APTeamMultiSigInitialized,
-  ConfirmationRevoked,
-  OwnerAdded,
-  OwnerRemoved,
-  RequireExecutionChanged,
-  TransactionConfirmed,
-  TransactionExecuted,
-  TransactionExpiryChanged,
-  TransactionSubmitted
+  MultiSig,
+  MultiSigOwner,
+  MultiSigType,
+  MultiSigTransaction,
+  TransactionConfirmation,
+  User,
 } from "../generated/schema"
+
+export function handleInitialized(event: InitializedEvent): void {
+  let ms = new MultiSig(event.params.msAddress);
+  event.params.owners.forEach(owner => {
+    // look up User or create a new one if dne
+    let user = User.load(owner);
+    if (user == null) {
+      user = new User(owner);
+      user.save();
+    }
+    let mso = new MultiSigOwner(ms.id.concatI32(user.id.toI32()))
+    mso.multiSig = ms;
+    mso.owner = user;
+    mso.save();
+  }
+  ms.transactionExpiry = event.params.transactionExpiry;
+  ms.requireExecution = event.params.requireExecution;
+  ms.approvalsRequired = event.params.approvalsRequired;
+  ms.multiSigType = MultiSigType.Generic;
+  ms.save();
+}
 
 export function handleApprovalsRequiredChanged(
   event: ApprovalsRequiredChangedEvent
 ): void {
-  let entity = new ApprovalsRequiredChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.approvalsRequired = event.params.approvalsRequired
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleConfirmationRevoked(
-  event: ConfirmationRevokedEvent
-): void {
-  let entity = new ConfirmationRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.transactionId = event.params.transactionId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleDeposit(event: DepositEvent): void {
-  let entity = new APTeamMultiSigDeposit(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new APTeamMultiSigInitialized(
-
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnerAdded(event: OwnerAddedEvent): void {
-  let entity = new OwnerAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnerRemoved(event: OwnerRemovedEvent): void {
-  let entity = new OwnerRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let ms = MultiSig.load(event.params.msAddress);
+  ms.approvalsRequired = event.params.approvalsRequired;
+  ms.save();
 }
 
 export function handleRequireExecutionChanged(
   event: RequireExecutionChangedEvent
 ): void {
-  let entity = new RequireExecutionChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.requireExecution = event.params.requireExecution
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let ms = MultiSig.load(event.params.msAddress);
+  ms.requireExecution = event.params.requireExecution;
+  ms.save();
 }
 
-export function handleTransactionConfirmed(
-  event: TransactionConfirmedEvent
+export function handleExpiryChanged(
+  event: ExpiryChangedEvent
 ): void {
-  let entity = new TransactionConfirmed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.transactionId = event.params.transactionId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let ms = MultiSig.load(event.params.msAddress);
+  ms.transactionExpiry = event.params.transactionExpiry;
+  ms.save();
 }
 
-export function handleTransactionExecuted(
-  event: TransactionExecutedEvent
-): void {
-  let entity = new TransactionExecuted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.transactionId = event.params.transactionId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function handleOwnerAdded(event: OwnerAddedEvent): void {
+  let ms = MultiSig.load(event.params.msAddress);
+  let msoId = ms.id.concatI32(event.params.owner.toI32();
+  let mso = MultiSigOwner.load(msoId);
+  if (mso) {
+    // throw error
+  }
+  let mso = new MultiSigOwner(msoId)
+  mso.multiSig = ms;
+  mso.owner = event.params.owner;
+  mso.save();
 }
 
-export function handleTransactionExpiryChanged(
-  event: TransactionExpiryChangedEvent
-): void {
-  let entity = new TransactionExpiryChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.transactionExpiry = event.params.transactionExpiry
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function handleOwnerRemoved(event: OwnerRemovedEvent): void {
+  store.remove("MultiSigOwner", event.params.msAddress.concatI32(event.params.owner.toI32());
 }
 
 export function handleTransactionSubmitted(
   event: TransactionSubmittedEvent
 ): void {
-  let entity = new TransactionSubmitted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.transactionId = event.params.transactionId
+  let tx = new MultiSigTransaction(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  tx.transactionId = event.params.transactionId;
+  tx.proposer = event.params.sender;
+  tx.executed: event.params.executed;
+  tx.expiry: event.params.expiry;
+  tx.blockTimestamp = event.block.timestamp;
+  tx.save();
+}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleTransactionConfirmed(
+  event: TransactionConfirmedEvent
+): void {
+  const tx = MultiSigTransaction.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  let txConf = TransactionConfirmation.load(tx.id.concatI32(event.params.owner.toI32()));
+  if (!txConf) {
+    txConf = new TransactionConfirmed(tx.id.concatI32(event.params.owner.toI32()));
+    txConf.transaction = tx;
+    txConf.owner = User.load(event.params.owner);
+  }
+  txConf.confirmed = true;
+  txConf.save();
+}
 
-  entity.save()
+export function handleTransactionConfirmationRevoked(
+  event: ConfirmationRevokedEvent
+): void {
+  const tx = MultiSigTransaction.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  let txConf = TransactionConfirmation.load(tx.id.concatI32(event.params.owner.toI32()));
+  txConf.confirmed = false;
+  txConf.save();
+}
+
+export function handleTransactionExecuted(
+  event: TransactionExecutedEvent
+): void {
+  let tx = MultiSigTransaction.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  tx.executed: event.params.executed;
+  tx.save();
 }
--- src/charity-applications.ts
+++ src/charity-applications.ts
@@ -1,291 +1,188 @@
 import {
+  Initialized as InitializedEvent,
+  ApprovalsRequiredChanged as ApprovalsRequiredChangedEvent,
+  RequireExecutionChanged as RequireExecutionChangedEvent,
+  ExpiryChanged as ExpiryChangedEvent,
+  OwnerAdded as OwnerAddedEvent,
+  OwnerRemoved as OwnerRemovedEvent,
+  TransactionSubmitted as TransactionSubmittedEvent
+  TransactionConfirmed as TransactionConfirmedEvent,
+  TransactionConfirmationRevoked as TransactionConfirmationRevokedEvent,
+  TransactionExecuted as TransactionExecutedEvent,
   ApplicationConfirmationRevoked as ApplicationConfirmationRevokedEvent,
   ApplicationConfirmed as ApplicationConfirmedEvent,
   ApplicationExecuted as ApplicationExecutedEvent,
   ApplicationProposed as ApplicationProposedEvent,
-  ApprovalsRequiredChanged as ApprovalsRequiredChangedEvent,
-  ConfirmationRevoked as ConfirmationRevokedEvent,
-  Deposit as DepositEvent,
-  GasSent as GasSentEvent,
-  Initialized as InitializedEvent,
-  OwnerAdded as OwnerAddedEvent,
-  OwnerRemoved as OwnerRemovedEvent,
-  RequireExecutionChanged as RequireExecutionChangedEvent,
-  SeedAssetSent as SeedAssetSentEvent,
-  TransactionConfirmed as TransactionConfirmedEvent,
-  TransactionExecuted as TransactionExecutedEvent,
-  TransactionExpiryChanged as TransactionExpiryChangedEvent,
-  TransactionSubmitted as TransactionSubmittedEvent
 } from "../generated/CharityApplications/CharityApplications"
 import {
-  ApplicationConfirmationRevoked,
-  ApplicationConfirmed,
-  ApplicationExecuted,
-  ApplicationProposed,
-  ApprovalsRequiredChanged,
-  CharityApplicationsDeposit,
-  CharityApplicationsInitialized,
-  ConfirmationRevoked,
-  GasSent,
-  OwnerAdded,
-  OwnerRemoved,
-  RequireExecutionChanged,
-  SeedAssetSent,
-  TransactionConfirmed,
-  TransactionExecuted,
-  TransactionExpiryChanged,
-  TransactionSubmitted
+  ApplicationsMultisig,
+  ApplicationProposal,
+  ApplicationConfirmation,
+  MultiSig,
+  MultiSigOwner,
+  MultiSigType,
+  MultiSigTransaction,
+  TransactionConfirmation,
+  User,
 } from "../generated/schema"
 
-export function handleApplicationConfirmationRevoked(
-  event: ApplicationConfirmationRevokedEvent
-): void {
-  let entity = new ApplicationConfirmationRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.proposalId = event.params.proposalId
-  entity.owner = event.params.owner
+export function handleInitialized(event: InitializedEvent): void {
+  let ms = new MultiSig(event.params.msAddress);
+  event.params.owners.forEach(owner => {
+    // look up User or create a new one if dne
+    let user = User.load(owner);
+    if (user == null) {
+      user = new User(owner);
+      user.save();
+    }
+    let mso = new MultiSigOwner(ms.id.concatI32(user.id.toI32()))
+    mso.multiSig = ms;
+    mso.owner = user;
+    mso.save();
+  }
+  ms.transactionExpiry = event.params.transactionExpiry;
+  ms.requireExecution = event.params.requireExecution;
+  ms.approvalsRequired = event.params.approvalsRequired;
+  ms.multiSigType = MultiSigType.Generic;
+  ms.save();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApplicationConfirmed(
-  event: ApplicationConfirmedEvent
-): void {
-  let entity = new ApplicationConfirmed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.proposalId = event.params.proposalId
-  entity.owner = event.params.owner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApplicationExecuted(
-  event: ApplicationExecutedEvent
-): void {
-  let entity = new ApplicationExecuted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.proposalId = event.params.proposalId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApplicationProposed(
-  event: ApplicationProposedEvent
-): void {
-  let entity = new ApplicationProposed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.proposalId = event.params.proposalId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  // setup the applications multisig varient
+  let appMs = new ApplicationsMultisig(event.params.msAddress);
+  appMs.seedSplitToLiquid: event.params.seedSplitToLiquid;
+  gasAmount: event.params.gasAmount;
+  seedAsset: event.params.seedAsset;
+  seedAmount: event.params.seedAmount;
+  appMs.save();
 }
 
 export function handleApprovalsRequiredChanged(
   event: ApprovalsRequiredChangedEvent
 ): void {
-  let entity = new ApprovalsRequiredChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.approvalsRequired = event.params.approvalsRequired
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleConfirmationRevoked(
-  event: ConfirmationRevokedEvent
-): void {
-  let entity = new ConfirmationRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.transactionId = event.params.transactionId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleDeposit(event: DepositEvent): void {
-  let entity = new CharityApplicationsDeposit(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleGasSent(event: GasSentEvent): void {
-  let entity = new GasSent(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.endowmentId = event.params.endowmentId
-  entity.member = event.params.member
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new CharityApplicationsInitialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnerAdded(event: OwnerAddedEvent): void {
-  let entity = new OwnerAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnerRemoved(event: OwnerRemovedEvent): void {
-  let entity = new OwnerRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let ms = MultiSig.load(event.params.msAddress);
+  ms.approvalsRequired = event.params.approvalsRequired;
+  ms.save();
 }
 
 export function handleRequireExecutionChanged(
   event: RequireExecutionChangedEvent
 ): void {
-  let entity = new RequireExecutionChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.requireExecution = event.params.requireExecution
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let ms = MultiSig.load(event.params.msAddress);
+  ms.requireExecution = event.params.requireExecution;
+  ms.save();
 }
 
-export function handleSeedAssetSent(event: SeedAssetSentEvent): void {
-  let entity = new SeedAssetSent(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.endowmentId = event.params.endowmentId
-  entity.asset = event.params.asset
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransactionConfirmed(
-  event: TransactionConfirmedEvent
+export function handleExpiryChanged(
+  event: ExpiryChangedEvent
 ): void {
-  let entity = new TransactionConfirmed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.transactionId = event.params.transactionId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let ms = MultiSig.load(event.params.msAddress);
+  ms.transactionExpiry = event.params.transactionExpiry;
+  ms.save();
 }
 
-export function handleTransactionExecuted(
-  event: TransactionExecutedEvent
-): void {
-  let entity = new TransactionExecuted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.transactionId = event.params.transactionId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function handleOwnerAdded(event: OwnerAddedEvent): void {
+  let ms = MultiSig.load(event.params.msAddress);
+  let msoId = ms.id.concatI32(event.params.owner.toI32();
+  let mso = MultiSigOwner.load(msoId);
+  if (mso) {
+    // throw error
+  }
+  let mso = new MultiSigOwner(msoId)
+  mso.multiSig = ms;
+  mso.owner = event.params.owner;
+  mso.save();
 }
 
-export function handleTransactionExpiryChanged(
-  event: TransactionExpiryChangedEvent
-): void {
-  let entity = new TransactionExpiryChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.transactionExpiry = event.params.transactionExpiry
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function handleOwnerRemoved(event: OwnerRemovedEvent): void {
+  store.remove("MultiSigOwner", event.params.msAddress.concatI32(event.params.owner.toI32());
 }
 
 export function handleTransactionSubmitted(
   event: TransactionSubmittedEvent
 ): void {
-  let entity = new TransactionSubmitted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.transactionId = event.params.transactionId
+  let tx = new MultiSigTransaction(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  tx.transactionId = event.params.transactionId;
+  tx.proposer = event.params.sender;
+  tx.executed: event.params.executed;
+  tx.expiry: event.params.expiry;
+  tx.blockTimestamp = event.block.timestamp;
+  tx.save();
+}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleTransactionConfirmed(
+  event: TransactionConfirmedEvent
+): void {
+  const tx = MultiSigTransaction.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  let txConf = TransactionConfirmation.load(tx.id.concatI32(event.params.owner.toI32()));
+  if (!txConf) {
+    txConf = new TransactionConfirmed(tx.id.concatI32(event.params.owner.toI32()));
+    txConf.transaction = tx;
+    txConf.owner = User.load(event.params.owner);
+  }
+  txConf.confirmed = true;
+  txConf.save();
+}
 
-  entity.save()
+export function handleTransactionConfirmationRevoked(
+  event: ConfirmationRevokedEvent
+): void {
+  const tx = MultiSigTransaction.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  let txConf = TransactionConfirmation.load(tx.id.concatI32(event.params.owner.toI32()));
+  txConf.confirmed = false;
+  txConf.save();
+}
+
+export function handleTransactionExecuted(
+  event: TransactionExecutedEvent
+): void {
+  let tx = MultiSigTransaction.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  tx.executed: event.params.executed;
+  tx.save();
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * ~~~~ APPLICATIONS HANDLERS ~~~~
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+export function handleApplicationProposed(
+  event: ApplicationProposedEvent
+): void {
+  const appMs = ApplicationsMultisig.load(event.params.msAddress);
+  proposal.multiSig = appMs;
+  proposal.charityName = event.params.charityName;
+  proposal.proposer = event.params.proposer;
+  proposal.executed = event.params.executed;
+  proposal.expiry = event.params.expiry;
+  proposal.blockTimestamp = event.block.timestamp;
+  proposal.save();
+}
+
+export function handleApplicationConfirmed(
+  event: ApplicationConfirmedEvent
+): void {
+  let app = ApplicationProposal.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  let appConf = TransactionConfirmation.load(app.id.concatI32(event.params.owner.toI32()));
+  if (!appConf) {
+    appConf = new TransactionConfirmed(app.id.concatI32(event.params.owner.toI32()));
+    appConf.transaction = app;
+    appConf.owner = User.load(event.params.owner);
+  }
+  appConf.confirmed = true;
+  appConf.save();
+}
+
+export function handleApplicationConfirmationRevoked(
+  event: ApplicationConfirmationRevokedEvent
+): void {
+  const app = ApplicationProposal.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  let appConf = TransactionConfirmation.load(app.id.concatI32(event.params.owner.toI32()));
+  appConf.confirmed = false;
+  appConf.save();
+}
+
+export function handleApplicationExecuted(
+  event: ApplicationExecutedEvent
+): void {
+  let app = ApplicationProposal.load(event.params.msAddress.concatI32(event.params.transactionId.toI32()));
+  app.executed: event.params.executed;
+  app.save();
 }

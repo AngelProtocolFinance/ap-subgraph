a53697c64f105767b81d32df1aca255285c07c66
Nenad <nenad@angelprotocol.io>
2023-08-22 14:46:05 +0200

    Separate deployments for Polygon and non-Polygon networks

---
+++ src/registrar.ts
@@ -0,0 +1,35 @@
+import {
+    StrategyApprovalUpdated as StrategyApprovalUpdatedEvent,
+    StrategyParamsUpdated as StrategyParamsUpdatedEvent,
+} from "../generated/LocalRegistrar/LocalRegistrar"
+import { Strategy } from "../generated/schema"
+import { StrategyApprovalState } from "./helpers"
+
+export function handleStrategyParamsUpdated(
+    event: StrategyParamsUpdatedEvent
+): void {
+    const strategy = new Strategy(event.params._strategyId)
+    strategy.network = event.params._network
+    strategy.state = getStrategyApprovalState(event.params._approvalState)
+    strategy.save()
+}
+
+export function handleStrategyApprovalUpdated(
+    event: StrategyApprovalUpdatedEvent
+): void {
+    const strategy = Strategy.load(event.params._strategyId)
+    if (strategy != null) {
+        strategy.state = getStrategyApprovalState(event.params._approvalState)
+        strategy.save()
+    }
+}
+
+function getStrategyApprovalState(value: number): string {
+    return value == StrategyApprovalState.NOT_APPROVED
+        ? "NOT_APPROVED"
+        : value == StrategyApprovalState.APPROVED
+            ? "APPROVED"
+            : value == StrategyApprovalState.WITHDRAW_ONLY
+                ? "WITHDRAW_ONLY"
+                : "DEPRECATED"
+}
